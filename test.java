de90c5e519274dfb3d31bcc45b4929465368462a
lkorca <lkorca@citycollege.sheffield.eu>
2019-11-16 22:32:36 +0100

    new edited file

--- test.java
+++ test.java
@@ -1,5 +1,6 @@
 public class test{
 	public static void main(String[] args) {
-		System.out.println("example");
+		System.out.println("albin kurti for prime minister");
+		System.out.println("e kryta");
 	}
 }
